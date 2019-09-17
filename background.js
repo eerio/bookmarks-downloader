6730dd4d72e1a159b583efe0b46e77d1db931128
Paweł Balawender <pbalawender101@gmail.com>
2019-09-17 22:19:43 +0200

    Rename to sth more descriptive

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ chrome.browserAction.onClicked.addListener(function() {
 		active: true
 	}, function(tab) {
 		chrome.tabs.create({
-			"url": "mypage.html"
+			"url": "index.html"
 		});
 	});
 });
